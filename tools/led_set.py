fabc67e56e0952292e1716c8db671253542a2ed6
Antoine Albertelli <antoine@antoinealb.net>
2018-02-27 23:52:03 +0100

    Adapt led_set for new RPC method

--- tools/led_set.py
+++ tools/led_set.py
@@ -6,36 +6,36 @@ Turns off or on LEDs on the board. Useful to test the RPC framework.
 import argparse
 import messages
 import socket
+import rpc
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("host", help="IP address of the board")
-    parser.add_argument("--port", "-p", type=int, default=10000,
-                        help="Port to connect to (default 10000)")
-
-    parser.add_argument("led", choices=['green', 'red', 'blue'],
-                        help="LED to set")
-    parser.add_argument("status", choices=['on', 'off'],
-                        help="LED status")
+    parser.add_argument(
+        "--port",
+        "-p",
+        type=int,
+        default=10000,
+        help="Port to connect to (default 10000)")
+    parser.add_argument(
+        "led", choices=['green', 'red', 'blue'], help="LED to set")
+    parser.add_argument("status", choices=['on', 'off'], help="LED status")
 
     return parser.parse_args()
 
+
 def led_set(conn, led, value):
-    header = messages.RPCRequestHeader(name="led_set").SerializeToString()
-    header_size = messages.MessageSize(bytes=len(header)).SerializeToString()
     led = messages.SetLedRequest.Led.Value(led.upper())
-    msg = messages.SetLedRequest(led=led, status=value).SerializeToString()
-    msg_size = messages.MessageSize(bytes=len(msg)).SerializeToString()
-    conn.send(header_size + header + msg_size + msg)
+    msg = messages.SetLedRequest(led=led, status=value)
+    conn.send(rpc.encode_request('led_set', msg))
+
 
 def main():
     args = parse_args()
-
     conn = socket.create_connection((args.host, args.port))
-
     led_set(conn, args.led, args.status == 'on')
 
-    print(conn.recv(1024))
 
 if __name__ == '__main__':
     main()
