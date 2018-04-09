20eab516bcb441128a5798f02ba834a372584fcd
Antoine Albertelli <antoine@antoinealb.net>
2018-04-09 17:19:04 +0200

    Use wget instead

--- .ci/install_bazel.sh
+++ .ci/install_bazel.sh
@@ -3,7 +3,7 @@
 set -eu
 
 echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" > /etc/apt/sources.list.d/bazel.list
-curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
+wget https://bazel.build/bazel-release.pub.gpg -O - | apt-key add -
 
 apt-get update
 apt-get install -y openjdk-8-jdk bazel
