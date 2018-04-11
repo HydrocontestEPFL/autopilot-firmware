1f12b31bdc6b9918ffc096db1cdfa556ba0ce522
Antoine Albertelli <antoine@antoinealb.net>
2018-04-11 16:56:30 +0200

    Fix for message drop on UDP
    Basically we divide the work of sending on UDP into two threads
    communicating through a queue.
    
    1. The first thread is responsible for reacting to a message sent on the
    bus. It must do so very quickly because all messages sent while it is
    processing are lost. In order to do so, it has a higher priority than
    most threads (HIGHPRIO). It encodes the message as protobuf, and sends
    it to the other thread for processing.
    2. The other thread waits for a message to come and sends it over UDP.
    It has normal priority and can take as long as it needs, since messages
    are stored into a buffer.

--- src/telemetry/udp_topic_broadcaster.c
+++ src/telemetry/udp_topic_broadcaster.c
@@ -1,12 +1,26 @@
 #include <ch.h>
 #include <lwip/api.h>
 #include "main.h"
+#include "error/error.h"
 #include "msgbus_protobuf.h"
 #include "udp_topic_broadcaster.h"
 
 static messagebus_watchgroup_t watchgroup;
 static MUTEX_DECL(watchgroup_lock);
 static CONDVAR_DECL(watchgroup_condvar);
+
+#define MSG_MAX_LENGTH 128
+#define MSG_BUF_SIZE 16
+
+struct encoded_message {
+    uint16_t len;
+    uint8_t buf[MSG_MAX_LENGTH];
+};
+
+static struct encoded_message msg_buffer[MSG_BUF_SIZE];
+static char *msg_mailbox_buf[MSG_BUF_SIZE];
+static MAILBOX_DECL(msg_mailbox, msg_mailbox_buf, MSG_BUF_SIZE);
+static MEMORYPOOL_DECL(msg_pool, sizeof(struct encoded_message), NULL);
 
 static void new_topic_cb(messagebus_t *bus, messagebus_topic_t *topic, void *arg)
 {
@@ -16,46 +30,71 @@ static void new_topic_cb(messagebus_t *bus, messagebus_topic_t *topic, void *arg
     messagebus_watchgroup_watch(&metadata->udp_watcher, &watchgroup, topic);
 }
 
-static void udp_topic_broadcast_thd(void *p)
+static void udp_topic_encode_thd(void *p)
 {
     (void)p;
 
     messagebus_topic_t *topic;
 
     uint8_t object_buf[512];
-    uint8_t msg_buf[256];
-    size_t encoded_size;
+
+    /* We need to have max priority to avoid skipping messages */
+    chThdSetPriority(HIGHPRIO);
+    chRegSetThreadName(__FUNCTION__);
 
     while (true) {
-        struct netconn *conn;
-        struct netbuf *buf;
+        struct encoded_message *msg = chPoolAlloc(&msg_pool);
 
-        topic = messagebus_watchgroup_wait(&watchgroup);
-
-        encoded_size = messagebus_encode_topic_message(topic,
-                                                       msg_buf,
-                                                       sizeof(msg_buf),
-                                                       object_buf,
-                                                       sizeof(object_buf));
-
-        if (encoded_size == 0) {
+        if (msg == NULL) {
+            WARNING("Dropping a messsage.");
             continue;
         }
 
-        conn = netconn_new(NETCONN_UDP);
+        topic = messagebus_watchgroup_wait(&watchgroup);
 
-        if (conn == NULL) {
+        msg->len = messagebus_encode_topic_message(topic,
+                                                   msg->buf,
+                                                   MSG_MAX_LENGTH,
+                                                   object_buf,
+                                                   sizeof(object_buf));
+
+        if (msg->len > 0) {
+            chMBPost(&msg_mailbox, (msg_t)msg, TIME_IMMEDIATE);
+        } else {
+            chPoolFree(&msg_pool, msg);
+        }
+    }
+}
+
+static void udp_topic_send_thd(void *p)
+{
+    (void)p;
+    chThdSetPriority(NORMALPRIO);
+    chRegSetThreadName(__FUNCTION__);
+
+    struct netconn *conn;
+    conn = netconn_new(NETCONN_UDP);
+
+    chDbgAssert(conn != NULL, "Could not create connection");
+
+    while (true) {
+        struct encoded_message *msg;
+        struct netbuf *buf;
+
+        msg_t res = chMBFetch(&msg_mailbox, (msg_t *)&msg, TIME_INFINITE);
+
+        if (res != MSG_OK) {
             continue;
         }
 
         buf = netbuf_new();
 
         if (buf == NULL) {
-            netconn_delete(conn);
+            chPoolFree(&msg_pool, msg);
             continue;
         }
 
-        netbuf_ref(buf, msg_buf, encoded_size);
+        netbuf_ref(buf, msg->buf, msg->len);
 
         // TODO: take those from parameter tree
         ip_addr_t addr;
@@ -65,15 +104,21 @@ static void udp_topic_broadcast_thd(void *p)
         netconn_sendto(conn, buf, &addr, port);
 
         netbuf_delete(buf);
-        netconn_delete(conn);
+        chPoolFree(&msg_pool, msg);
     }
 }
 
 void udp_topic_broadcast_start(void)
 {
-    static THD_WORKING_AREA(wa, 2048);
+    chPoolLoadArray(&msg_pool, msg_buffer, MSG_BUF_SIZE);
+
     static messagebus_new_topic_cb_t cb;
     messagebus_watchgroup_init(&watchgroup, &watchgroup_lock, &watchgroup_condvar);
     messagebus_new_topic_callback_register(&bus, &cb, new_topic_cb, NULL);
-    chThdCreateStatic(wa, sizeof(wa), HIGHPRIO, udp_topic_broadcast_thd, NULL);
+
+    static THD_WORKING_AREA(encode_wa, 2048);
+    chThdCreateStatic(encode_wa, sizeof(encode_wa), HIGHPRIO, udp_topic_encode_thd, NULL);
+
+    static THD_WORKING_AREA(send_wa, 2048);
+    chThdCreateStatic(send_wa, sizeof(send_wa), NORMALPRIO, udp_topic_send_thd, NULL);
 }
