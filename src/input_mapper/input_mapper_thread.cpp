665b27cca97053b974a1f21d36581780561430a3
Antoine Albertelli <antoine@antoinealb.net>
2018-04-24 17:20:58 +0200

    Use msgbus c++ interface

--- src/input_mapper/input_mapper_thread.cpp
+++ src/input_mapper/input_mapper_thread.cpp
@@ -1,5 +1,6 @@
 #include <ch.h>
 #include <hal.h>
+#include "msgbus/messagebus.h"
 #include "main.h"
 #include "msgbus_protobuf.h"
 #include "input_mapper_thread.h"
@@ -10,7 +11,6 @@
 static void input_mapper_thread(void *p)
 {
     (void)p;
-    messagebus_topic_t *sbus_topic;
     static TOPIC_DECL(rcinput, RemoteControlInput);
     static parameter_namespace_t mapper_ns;
     static input_mapper_t mapper;
@@ -19,14 +19,14 @@ static void input_mapper_thread(void *p)
     parameter_namespace_declare(&mapper_ns, &parameter_root, "input_map");
     input_mapper_init(&mapper, &mapper_ns);
 
-    sbus_topic = messagebus_find_topic_blocking(&bus, "sbus");
+    messagebus::TopicWrapper<RemoteControlInput> rc_input_topic (&rcinput.topic);
+    auto sbus_topic = messagebus::find_topic<SBUSPacket>(bus, "sbus");
 
     while (1) {
-        SBUSPacket packet;
         RemoteControlInput result;
-        messagebus_topic_wait(sbus_topic, &packet, sizeof(SBUSPacket));
+        auto packet = sbus_topic.wait();
         input_mapper_map(&mapper, &packet, &result);
-        messagebus_topic_publish(&rcinput.topic, &result, sizeof(result));
+        rc_input_topic.publish(result);
     }
 }
 
